78652d78d00e30e0e6cb6724d20a5142edeca4c6
felipe.angelo <felipe.angelo@ymail.com>
2024-06-25 22:48:27 -0300

    Desenvolvida a lógica de classificar o herói

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Hello World")
+let heroLuckyNumber = Math.floor(Math.random()*5) + 1
+let heroName = ""
+let heroExp = Math.floor(Math.random()*11000)
+let heroCategory = ""
+
+switch(heroLuckyNumber){
+    case 1:
+        heroName = "Adalbert"
+    break
+    case 2:
+        heroName = "Bernard"
+    break
+    case 3:
+        heroName = "Cassius"
+    break
+    case 4:
+        heroName = "Dandarion"
+    break
+    case 5:
+        heroName = "Evander"
+    break
+}
+
+if (heroExp < 1000) {
+    heroCategory = "Ferro"
+} else if (heroExp < 2000) {
+    heroCategory = "Bronze"
+} else if (heroExp < 5000) {
+    heroCategory = "Prata"
+} else if (heroExp < 7000) {
+    heroCategory = "Ouro"
+} else if (heroExp < 8000) {
+    heroCategory = "Platina"
+} else if (heroExp < 9000) {
+    heroCategory = "Ascendente"
+} else if (heroExp < 10001) {
+    heroCategory = "Imortal"
+} else {
+    heroCategory = "Radiante"
+}
+
+console.log("O herói de nome " + heroName + " está no nível de " + heroCategory)
+console.log("Total de experência: " + heroExp)
